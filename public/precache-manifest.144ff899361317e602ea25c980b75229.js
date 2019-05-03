f2281d789583b72418bb7bf6caac34c0664d48f6
Adrien Montbroussous <adrien.montbroussous@insee.fr>
2019-05-03 14:34:49 +0200

    test copie build dans public

---
+++ public/precache-manifest.144ff899361317e602ea25c980b75229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947c6e7fae1c79a158d2b40ca6e4115d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "047cf16b2141454afeca",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "047cf16b2141454afeca",
+    "url": "/static/js/main.993b95ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
